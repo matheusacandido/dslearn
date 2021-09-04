b0871fc65d80d717cb3a54b7a640e9d24231291d
matheusacandido <matheusacandido@hotmail.com>
2021-09-03 22:23:14 -0300

    User Role

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idLong;
+	private String authority;
+
+	public Role() {
+	}
+
+	public Role(Long idLong, String authority) {
+		super();
+		this.idLong = idLong;
+		this.authority = authority;
+	}
+
+	public Long getIdLong() {
+		return idLong;
+	}
+
+	public void setIdLong(Long idLong) {
+		this.idLong = idLong;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idLong == null) ? 0 : idLong.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (idLong == null) {
+			if (other.idLong != null)
+				return false;
+		} else if (!idLong.equals(other.idLong))
+			return false;
+		return true;
+	}
+
+}
