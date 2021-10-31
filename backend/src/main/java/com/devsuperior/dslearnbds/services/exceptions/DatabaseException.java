6aaa75b153ed0d5f81188728ef74ced76e509422
matheusacandido <matheusacandido@hotmail.com>
2021-10-31 15:47:48 -0300

    exceptions, validation, security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class DatabaseException extends DataIntegrityViolationException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
